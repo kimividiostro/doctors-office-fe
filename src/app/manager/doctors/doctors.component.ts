65758529e346635a821635f555961e5ede1d203d
Milan <milance@pop-os.localdomain>
2023-08-29 00:31:31 +0200

    Created doctor registration form... almost...

--- src/app/manager/doctors/doctors.component.ts
+++ src/app/manager/doctors/doctors.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Doctor } from 'src/app/models/doctor';
+import { Specialization } from 'src/app/models/specialization';
 import { DoctorService } from 'src/app/services/doctor.service';
+import { ManagerService } from 'src/app/services/manager.service';
 
 @Component({
   selector: 'app-doctors',
@@ -10,15 +13,60 @@ import { DoctorService } from 'src/app/services/doctor.service';
 export class DoctorsComponent implements OnInit {
 
   doctors: Doctor[];
-  constructor(private doctorService: DoctorService) { }
+  specializations: Specialization[];
+  passwordRegex = /^(?!.*(.)\1)(?=.*[!@#$%^&*()\-_=+<>\?])(?=[A-Za-z])(?=.*[A-Z])(?=.*\d).{8,14}$/;
+
+  addDoctorForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    address: ['', Validators.required],
+    phone: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern(this.passwordRegex)]],
+    licenceNumber: ['', Validators.required],
+    officeDepartment: ['', Validators.required],
+    specialization: ['', Validators.required],
+    image: ['', Validators.required]
+  })
+  constructor(private doctorService: DoctorService, private fb: FormBuilder, private managerService: ManagerService) { }
 
   ngOnInit(): void {
     this.getDoctors();
+    this.getSpecializations();
   }
 
   getDoctors() {
     this.doctorService.getDoctors().subscribe(
       res => this.doctors = res.doctors
+    )
+  }
+
+  onFileSelected(event) {
+    const filePath = event.target.files[0];
+    if(filePath) {
+      const image = new Image();
+      image.src = window.URL.createObjectURL(filePath);
+      image.onload = () => {
+        if(image.width < 100 || image.height < 100
+          || image.width > 300 || image.height > 300){
+            this.addDoctorForm.controls.image.setErrors({incorrect: true});
+          }
+        else {
+          this.addDoctorForm.controls.image.setErrors(null);
+        }
+      }
+    }
+  }
+
+  addNewDoctor() {
+    if(this.addDoctorForm.valid) {
+
+    }
+  }
+
+  getSpecializations() {
+    this.managerService.getSpecializations().subscribe(
+      res => this.specializations = res.specializations
     )
   }
 }
